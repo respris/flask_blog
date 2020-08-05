70362d63481e8e202876a6475167ef535cfe94d5
Rafael Espinar <respinar@cartodb.com>
2020-08-05 20:46:19 +0200

    templates and views

--- blog.py
+++ blog.py
@@ -18,5 +18,14 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+# functions for the views
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
